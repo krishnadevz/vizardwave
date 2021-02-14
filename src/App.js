61b6289fbea4438ea9087de194ac3035e5e51222
rushi718 <41866310+rushi718@users.noreply.github.com>
2021-02-14 19:55:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,99 +1,153 @@
-import React, { useRef, useEffect } from "react";
-import "./App.css";
+///////// NEW STUFF ADDED USE STATE
+import React, { useRef, useState, useEffect } from "react";
+///////// NEW STUFF ADDED USE STATE
+
+// import logo from './logo.svg';
 import * as tf from "@tensorflow/tfjs";
-
-import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
-import { drawMesh } from "./utilities";
+import "./App.css";
+import { drawHand } from "./utilities";
 
+import {loveYouGesture} from "./LoveYou"; 
 
-  //setup references
-  function App() {
-    const webcamRef = useRef(null);
-    const canvasRef = useRef(null);
+///////// NEW STUFF IMPORTS
+import * as fp from "fingerpose";
+import victory from "./victory.png";
+import thumbs_up from "./thumbs_up.png";
+///////// NEW STUFF IMPORTS
 
-    //  Load posenet
-    const runFacemesh = async () => {
-      // OLD MODEL
-      // const net = await facemesh.load({
-      //   inputResolution: { width: 640, height: 480 },
-      //   scale: 0.8,
-      // });
-      const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
-      setInterval(() => {
-        detect(net);
-      }, 10);
-    };
-  
-    const detect = async (net) => {
-      if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-      ) {
-        // Get Video Properties
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
-  
-        // Set video width
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
-  
-        // Set canvas width
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
-  
-        // Make Detections
-        // OLD MODEL
-        //       const face = await net.estimateFaces(video);
-        // NEW MODEL
-        const face = await net.estimateFaces({input:video});
-        console.log(face);
-  
-        // Get canvas context
-        const ctx = canvasRef.current.getContext("2d");
-        requestAnimationFrame(()=>{drawMesh(face, ctx)});
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  ///////// NEW STUFF ADDED STATE HOOK
+  const [emoji, setEmoji] = useState(null);
+  const images = { thumbs_up: thumbs_up, victory: victory };
+  ///////// NEW STUFF ADDED STATE HOOK
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      // console.log(hand);
+
+      ///////// NEW STUFF ADDED GESTURE HANDLING
+
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+          loveYouGesture
+        ]);
+        const gesture = await GE.estimate(hand[0].landmarks, 4);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          console.log(gesture.gestures);
+
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.confidence
+          );
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+          // console.log(gesture.gestures[maxConfidence].name);
+          setEmoji(gesture.gestures[maxConfidence].name);
+          console.log(emoji);
+        }
       }
-    };
-  
-    useEffect(()=>{runFacemesh()}, []);
-  
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Webcam
-            ref={webcamRef}
+
+      ///////// NEW STUFF ADDED GESTURE HANDLING
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  useEffect(()=>{runHandpose()},[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        {/* NEW STUFF */}
+        {emoji !== null ? (
+          <img
+            src={images[emoji]}
             style={{
               position: "absolute",
               marginLeft: "auto",
               marginRight: "auto",
-              left: 0,
+              left: 400,
+              bottom: 500,
               right: 0,
               textAlign: "center",
-              zindex: 9,
-              width: 640,
-              height: 480,
+              height: 100,
             }}
           />
-  
-          <canvas
-            ref={canvasRef}
-            style={{
-              position: "absolute",
-              marginLeft: "auto",
-              marginRight: "auto",
-              left: 0,
-              right: 0,
-              textAlign: "center",
-              zindex: 9,
-              width: 640,
-              height: 480,
-            }}
-          />
-        </header>
-      </div>
-    );
-  }
-  
-  export default App;
+        ) : (
+          ""
+        )}
+
+        {/* NEW STUFF */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
